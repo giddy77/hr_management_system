1aa80e9dec51be628aa9212e889f92139ba22b19
giddy77 <giddysipoi@gmail>
2024-01-25 01:01:33 +0300

    first commit

---
+++ app/Http/Controllers/OnboardingTaskController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OnboardingTask;
+use Illuminate\Http\Request;
+
+class OnboardingTaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(OnboardingTask $onboardingTask)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(OnboardingTask $onboardingTask)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, OnboardingTask $onboardingTask)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(OnboardingTask $onboardingTask)
+    {
+        //
+    }
+}
